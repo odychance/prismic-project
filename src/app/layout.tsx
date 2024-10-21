333c5fc4fb5bd7af4f4d715cba64f29fae33d5b7
OddyChance <odychance96@gmail.com>
2024-10-21 11:17:04 -0300

    feat: Add Prismic Preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito, Nunito_Sans} from 'next/font/google'
 import clsx from "clsx";
-import { createClient } from "@/prismicio";
+import { createClient, repositoryName } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -50,6 +51,8 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50" />
+      
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
