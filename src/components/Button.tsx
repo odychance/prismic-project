ebe420f8a89103e4dc1d060c44a4ac6841bf34d7
OddyChance <odychance96@gmail.com>
2024-10-23 10:03:07 -0300

    feat: restore Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({
                 "block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider",
                 className
             )}
-            {...restProps as any}
+            {...restProps}
         /> 
     )
 }
